abeaf204c1f07f75eb94f5ecdc712e756c3634ec
MVDam <47767573t@iespoblenou.org>
2016-03-05 18:46:03 +0100

    Factory acabado

--- controller.js
+++ controller.js
@@ -3,29 +3,31 @@
  */
 
 
-app.controller("ChatCtrl", ["$scope", "chatMessages",
-    // we pass our new chatMessages factory into the controller
-    function($scope, chatMessages, chatUsers, $firebaseObject, $firebaseArray) {
+app.controller("TweetCtrl", ["$scope", "getUser", 
+    ,function($scope, ) {
 
         //Inicializamos lo que mostramos
         $scope.user = "hispteric";
-        $scope.messages = chatMessages;
-        // we add chatMessages array to the scope to be used in our ng-repeat
+        $scope.tweets = chatTweets();
+
+
+        $scope.changeUser = function() {
+            $scope.user = $scope.userNew;
+        }
 
 
         //Metodo para crear nuevo mensajes que añade al array de mensajes; llamado ng-submit
-        $scope.addMessage = function() {
-            // calling $add on a synchronized array is like Array.push(),
-            // except that it saves the changes to our database!
-            $scope.messages.$add({
+        $scope.addTweet = function() {
+
+            $scope.tweets.$add({
                 user: $scope.user,
                 text: $scope.message
             });
         };
 
         //TODO: definir este elemento para devuelva array con tweets segun usuario pedido
-        $scope.buscar = function(){
-            $scope.messages = chatUsers;
+        $scope.seekUser() = function(){
+
         }
 
 
--- factory.js
+++ factory.js
@@ -2,25 +2,53 @@
  * Created by 47767573t on 25/02/16.
  */
 
-app.factory("chatMessages", ["$scope", "$firebaseArray"
-    ,function($scope, $firebaseArray, $firebaseObject) {
-        // create a reference to the database location where we will store our data
+/**
+ * factory para peticion de usuario
+ */
+app.factory("getUsuario", ["$firebaseObject"
+    ,function($firebaseObject) {
+        return function(usuario) {
 
-        var refTweet = new Firebase("https://ecaibtweet.firebaseio.com/tweets")
+            //Creamos la peticion de usuarios a firebase
+            var usuarioRef = new Firebase("https://ecaibtweet.firebaseio.com/users");
 
-        // this uses AngularFire to create the synchronized array
-        return $firebaseArray(refTweet);
+            //Tras la peticion devolvemos objeto usuario de firebase y devolvemos nombre y descripcion
+            return {nom: $firebaseObject(usuarioRef.child(usuario).child("name"))
+                ,desc: $firebaseObject(usuarioRef.child(usuari).child("description"))};
+
+        };
     }
 ]);
 
-app.factory("chatUsers", ["$scope", "$firebaseArray"
-    ,function($scope, $firebasearray, $firebaseObject){
+/**
+ * factory para peticion de tweets de usuario
+ */
+app.factory("getTweets", ["$firebaseArray"
+    ,function($firebaseArray) {
+        return function(usuario) {
 
-        var refUser = new Firebase("https://ecaibtweet.firebaseio.com/users");
+            //Creamos la peticion de usuarios a firebase
+            var tweetRef = new Firebase("https://ecaibtweet.firebaseio.com/users");
 
-        var query = refUser.equalTo($scope.userId);
-
-
-        return;
+            //Tras la peticion devolvemos un arrayList de tweets del usuario
+            return $firebaseArray(tweetRef.child(usuario).child("tweets"));
+        };
     }
 ]);
+
+/**
+ * factory para peticion de tweets de usuario
+ */
+app.factory("getUsuariosSeguidos", ["$firebaseArray", "$firebaseObject"
+    ,function($firebaseArray) {
+        return function(usuario) {
+
+            //Creamos la peticion de usuarios a firebase
+            var tweetRef = new Firebase("https://ecaibtweet.firebaseio.com/users");
+
+            //Tras la peticion devolvemos un arrayList usuarios que sigue
+            return $firebaseArray(tweetRef.child(usuario).child("following"));
+        };
+    }
+]);
+
